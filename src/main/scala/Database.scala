3f696ded699e54316fe35ea89458b854b08d74f5
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-10-14 12:16:47 +0200

    Add database module

---
+++ src/main/scala/Database.scala
@@ -0,0 +1,104 @@
+package com.github.lsund.gamestorer
+
+import scala.concurrent.ExecutionContext
+import doobie.implicits._
+import doobie.Update0
+import doobie.Transactor
+import doobie.Update
+import doobie.ConnectionIO
+import cats.effect.IO
+import cats._
+import cats.data._
+import cats.implicits._
+
+class Database {
+
+  case class Metadata(key: String, value: String) {}
+
+  case class Turn(number: Int, white: String, black: String) {}
+
+  case class Game(metadata: List[Metadata], turns: List[Turn], score: String) {}
+
+  def resetDatabase(xa: Transactor.Aux[IO, Unit]) = {
+    val drop =
+      sql"""
+         DROP TABLE IF EXISTS Turn;
+         DROP TABLE IF EXISTS Metadata;
+         DROP TABLE IF EXISTS Game;
+      """.update.run
+
+    val create =
+      sql"""
+         CREATE TABLE Turn (
+           id   SERIAL PRIMARY KEY,
+           gameid INT NOT NULL,
+           number INT NOT NULL,
+           white TEXT NOT NULL,
+           black TEXT NOT NULL
+         );
+         CREATE TABLE Metadata (
+           id   SERIAL PRIMARY KEY,
+           gameid INT NOT NULL,
+           key TEXT NOT NULL,
+           value TEXT NOT NULL
+         );
+         CREATE TABLE Game (
+           id   SERIAL PRIMARY KEY,
+           score TEXT NOT NULL
+         );
+      """.update.run
+    (drop, create).mapN(_ + _).transact(xa).unsafeRunSync
+  }
+
+  def transactor(): Transactor.Aux[IO, Unit] = {
+    implicit val cs = IO.contextShift(ExecutionContext.global)
+    return Transactor.fromDriverManager[IO](
+      "org.postgresql.Driver",
+      "jdbc:postgresql:chessgame",
+      "postgres",
+      ""
+    )
+  }
+
+  def turnToTuple(gameid: Int, turn: Turn): (Int, Int, String, String) = {
+    return (gameid, turn.number, turn.white, turn.black)
+  }
+
+  def metadataToTuple(
+      gameid: Int,
+      metadata: Metadata
+  ): (Int, String, String) = {
+    (gameid, metadata.key, metadata.value)
+  }
+
+  def insertTurns(gameid: Int, turns: List[Turn]): ConnectionIO[Int] = {
+    val sql =
+      "insert into turn (gameid, number, white, black) values (?, ?, ?, ?)"
+    Update[(Int, Int, String, String)](sql)
+      .updateMany(turns.map(x => turnToTuple(1, x)))
+  }
+
+  def insertMetadata(
+      gameid: Int,
+      metadata: List[Metadata]
+  ): ConnectionIO[Int] = {
+    val sql =
+      "insert into metadata (gameid, key, value) values (?, ?, ?)"
+    Update[(Int, String, String)](sql)
+      .updateMany(metadata.map(x => metadataToTuple(1, x)))
+  }
+
+  def insertGame(xa: Transactor.Aux[IO, Unit], game: Game) {
+    val y = xa.yolo
+    import y._
+    val score = game.score
+    val id :: _ = sql"insert into game (score) values ($score) RETURNING id;"
+      .query[Int]
+      .to[List]
+      .transact(xa)
+      .unsafeRunSync
+      .take(1)
+    insertTurns(id, game.turns).quick.unsafeRunSync
+    insertMetadata(id, game.metadata).quick.unsafeRunSync
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,16 +6,6 @@ import java.util.Properties
 import org.apache.kafka.clients.consumer._
 import org.apache.kafka.clients.producer._
 import scala.collection.JavaConverters._
-import doobie.Update0
-import doobie.Transactor
-import doobie.Update
-import doobie.ConnectionIO
-import doobie.implicits._
-import cats.effect.IO
-import scala.concurrent.ExecutionContext
-import cats._
-import cats.data._
-import cats.implicits._
 
 object Main extends App {
 
@@ -67,100 +57,17 @@ object Main extends App {
   val message = consumeMessage(consumer, "game")
   consumer.close()
 
-  case class Metadata(key: String, value: String) {}
+  val decodedGame = decode[db.Game](message)
 
-  case class Turn(number: Int, white: String, black: String) {}
+  val db = new Database()
 
-  case class Game(metadata: List[Metadata], turns: List[Turn], score: String) {}
+  val xa = db.transactor()
 
-  val decodedGame = decode[Game](message)
-
-  def resetDatabase(xa: Transactor.Aux[IO, Unit]) = {
-    val drop =
-      sql"""
-         DROP TABLE IF EXISTS Turn;
-         DROP TABLE IF EXISTS Metadata;
-         DROP TABLE IF EXISTS Game;
-      """.update.run
-
-    val create =
-      sql"""
-         CREATE TABLE Turn (
-           id   SERIAL PRIMARY KEY,
-           gameid INT NOT NULL,
-           number INT NOT NULL,
-           white TEXT NOT NULL,
-           black TEXT NOT NULL
-         );
-         CREATE TABLE Metadata (
-           id   SERIAL PRIMARY KEY,
-           gameid INT NOT NULL,
-           key TEXT NOT NULL,
-           value TEXT NOT NULL
-         );
-         CREATE TABLE Game (
-           id   SERIAL PRIMARY KEY,
-           score TEXT NOT NULL
-         );
-      """.update.run
-    (drop, create).mapN(_ + _).transact(xa).unsafeRunSync
-  }
-
-  def transactor(): Transactor.Aux[IO, Unit] = {
-    implicit val cs = IO.contextShift(ExecutionContext.global)
-    return Transactor.fromDriverManager[IO](
-      "org.postgresql.Driver",
-      "jdbc:postgresql:chessgame",
-      "postgres",
-      ""
-    )
-  }
-
-  val xa = transactor()
-
-  val y = xa.yolo
-  import y._
-
-  def turnToTuple(gameid: Int, turn: Turn): (Int, Int, String, String) = {
-    return (gameid, turn.number, turn.white, turn.black)
-  }
-
-  def metadataToTuple(
-      gameid: Int,
-      metadata: Metadata
-  ): (Int, String, String) = {
-    (gameid, metadata.key, metadata.value)
-  }
-
-  def insertTurns(gameid: Int, turns: List[Turn]): ConnectionIO[Int] = {
-    val sql =
-      "insert into turn (gameid, number, white, black) values (?, ?, ?, ?)"
-    Update[(Int, Int, String, String)](sql)
-      .updateMany(turns.map(x => turnToTuple(1, x)))
-  }
-
-  def insertMetadata(
-      gameid: Int,
-      metadata: List[Metadata]
-  ): ConnectionIO[Int] = {
-    val sql =
-      "insert into metadata (gameid, key, value) values (?, ?, ?)"
-    Update[(Int, String, String)](sql)
-      .updateMany(metadata.map(x => metadataToTuple(1, x)))
-  }
-
-  resetDatabase(xa)
+  db.resetDatabase(xa)
 
   decodedGame match {
     case Left(error) => println("Error")
-    case Right(Game(metadata, turns, score)) =>
-      val id :: _ = sql"insert into game (score) values ($score) RETURNING id;"
-        .query[Int]
-        .to[List]
-        .transact(xa)
-        .unsafeRunSync
-        .take(1)
-      insertTurns(id, turns).quick.unsafeRunSync
-      insertMetadata(id, metadata).quick.unsafeRunSync
+    case Right(game) =>
+      db.insertGame(xa, game)
   }
 }
