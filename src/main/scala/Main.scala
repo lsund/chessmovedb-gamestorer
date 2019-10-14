d4a4b3f889aa2a6732c1c9a67fddd14700eaf2f7
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-10-14 14:55:31 +0200

    Gracefully shut down on ctl-c

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,12 @@
 package com.github.lsund.chessmovedb_gamestorer
 
+import java.lang.Runtime
 import java.util
 import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
 import java.util.Properties
 import org.apache.kafka.clients.consumer._
 import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.errors.WakeupException
 import scala.collection.JavaConverters._
 
 object Main extends App {
@@ -40,34 +42,58 @@ object Main extends App {
 
   def consumeMessage(
       consumer: KafkaConsumer[String, String],
-      topic: String
+      topic: String,
+      db: Database
   ): String = {
+    val xa = db.transactor()
     consumer.subscribe(util.Arrays.asList(topic))
     while (true) {
       val record = consumer.poll(1000).asScala
-      for (data <- record.iterator)
-        return data.value()
+      for (data <- record.iterator) {
+        val message = data.value()
+        val decodedGame = decode[db.Game](message)
+        decodedGame match {
+          case Left(error) =>
+            println("Could not parse Kafka message:" + message)
+          case Right(game) =>
+            db.insertGame(xa, game)
+            println("Consumed and inserted game")
+        }
+      }
     }
     return ""
   }
 
   // val producer = makeKafkaProducer()
 
-  val consumer = makeKafkaConsumer()
-  val message = consumeMessage(consumer, "game")
-  consumer.close()
-
-  val decodedGame = decode[db.Game](message)
-
   val db = new Database()
+  val consumer = makeKafkaConsumer()
 
-  val xa = db.transactor()
-
-  db.resetDatabase(xa)
-
-  decodedGame match {
-    case Left(error) => println("Error")
-    case Right(game) =>
-      db.insertGame(xa, game)
+  val gameConsumption = new Thread {
+    override def run {
+      try {
+        consumeMessage(consumer, "game", db)
+      } catch {
+        case e: WakeupException =>
+        // Ignore
+      } finally {
+        consumer.close()
+      }
+    }
   }
+  gameConsumption.start
+
+  val mainThread = Thread.currentThread
+  Runtime.getRuntime
+    .addShutdownHook(new Thread() {
+      override def run {
+        consumer.wakeup
+        try {
+          mainThread.join
+        } catch {
+          case e: InterruptedException =>
+            println("Thread interrupted")
+        }
+      }
+    });
 }
